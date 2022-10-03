db20e0a8032da7331a53be6036b452142377ad12
Yevhenii Yakovenko <yakovenkob@gmail.com>
2022-10-03 23:01:26 +0300

    two task finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+refs.start.addEventListener('click', createColor);
+refs.stop.addEventListener('click', stopCreateColor);
+
+let interval = false;
+let timerId = null;
+
+function createColor()  {
+    if (interval) {
+        return;
+    } else {
+        timerId = setInterval(() => {
+            refs.body.style.backgroundColor  = getRandomHexColor();
+        }, 1000);
+        interval = true;
+    };
+};
+
+function stopCreateColor() {
+    interval = false;
+    clearInterval(timerId);
+};
